7e88380e5cf8606e53417e4359a757c7ac4d0100
Oliver Marcusson <oliver.marcusson@elev.ga.ntig.se>
2022-12-10 02:22:44 +0100

    updated script, added new feature, updated roleplay prompt

--- userscript.user.js
+++ userscript.user.js
@@ -11,6 +11,7 @@
 
 const jailbreakUrl = 'https://raw.githubusercontent.com/OliverMarcusson/ChatGPT-Fun/main/jailbreak';
 const rpUrl = 'https://raw.githubusercontent.com/OliverMarcusson/ChatGPT-Fun/main/roleplay';
+const multiUrl = 'https://raw.githubusercontent.com/OliverMarcusson/ChatGPT-Fun/main/eclipsemultitool';
 const btns = [];
 const removeBtns = btns => {
     btns.forEach(btn => {
@@ -41,8 +42,14 @@ window.addEventListener("load", async () => {
     rpBtn.setAttribute("class", "btn flex gap-2 justify-center btn-neutral");
     btns.push(rpBtn);
 
+    const multiBtn = document.createElement("button");
+    rpBtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" fill="none" stroke="#FFFFFF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><line x1="4" y1="21" x2="4" y2="14"></line><line x1="4" y1="10" x2="4" y2="3"></line><line x1="12" y1="21" x2="12" y2="12"></line><line x1="12" y1="8" x2="12" y2="3"></line><line x1="20" y1="21" x2="20" y2="16"></line><line x1="20" y1="12" x2="20" y2="3"></line><line x1="1" y1="14" x2="7" y2="14"></line><line x1="9" y1="8" x2="15" y2="8"></line><line x1="17" y1="16" x2="23" y2="16"></line></svg>Multitool (Made by Eclipse)'; // set the text of the button
+    rpBtn.setAttribute("class", "btn flex gap-2 justify-center btn-neutral");
+    btns.push(multiBtn);
+
     btnArray.appendChild(jailbreakBtn);
     btnArray.appendChild(rpBtn);
+    btnArray.appendChild(multiBtn);
 
     jailbreakBtn.addEventListener("click", async () => {
         await fetch(jailbreakUrl)
@@ -58,6 +65,18 @@ window.addEventListener("load", async () => {
 
     rpBtn.addEventListener("click", async () => {
         await fetch(rpUrl)
+        .then(async response => {
+            return await response.text();
+        })
+        .then(text => {
+            document.querySelector('[data-id="root"]').innerText = text;
+            document.querySelector('[data-id="root"]').nextSibling.click();
+            removeBtns(btns);
+        });
+    });
+    
+    multiBtn.addEventListener("click", async () => {
+        await fetch(multiUrl)
         .then(async response => {
             return await response.text();
         })
