a5184c3dd48ad54d9713511c75f71c86ecb4826c
Oliver Marcusson <oliver.marcusson@elev.ga.ntig.se>
2022-12-10 01:31:53 +0100

    fixed bug in userscript

--- userscript.user.js
+++ userscript.user.js
@@ -21,7 +21,15 @@ const removeBtns = btns => {
 window.addEventListener("load", async () => {
 
     console.log("Script loaded.")
-    const btnArray = document.getElementsByClassName('w-full flex gap-2 justify-center mb-3')[0];
+    let btnArray;
+    try {
+        btnArray = document.getElementsByClassName('w-full flex gap-2 justify-center mb-3')[0];
+    }
+    catch {
+        btnArray = document.createElement("div");
+        btnArray.setAttribute("class", "w-full flex gap-2 justify-center mb-3");
+        document.querySelector("main").appendChild(btnArray);
+    }
     console.log(btnArray);
     const jailbreakBtn = document.createElement("button"); // create a button element
     jailbreakBtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" fill="none" stroke="#FFFFFF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><rect x="3" y="11" width="18" height="11" rx="2" ry="2"></rect><path d="M7 11V7a5 5 0 0 1 9.9-1"></path></svg>Jailbreak'; // set the text of the button
