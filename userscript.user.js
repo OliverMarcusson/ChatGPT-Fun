28ec77ab546269292e04750b2f536b2562956c06
Oliver <106731065+OliverMarcusson@users.noreply.github.com>
2022-12-10 02:25:09 +0100

    Update userscript.user.js

--- userscript.user.js
+++ userscript.user.js
@@ -43,8 +43,8 @@ window.addEventListener("load", async () => {
     btns.push(rpBtn);
 
     const multiBtn = document.createElement("button");
-    rpBtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" fill="none" stroke="#FFFFFF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><line x1="4" y1="21" x2="4" y2="14"></line><line x1="4" y1="10" x2="4" y2="3"></line><line x1="12" y1="21" x2="12" y2="12"></line><line x1="12" y1="8" x2="12" y2="3"></line><line x1="20" y1="21" x2="20" y2="16"></line><line x1="20" y1="12" x2="20" y2="3"></line><line x1="1" y1="14" x2="7" y2="14"></line><line x1="9" y1="8" x2="15" y2="8"></line><line x1="17" y1="16" x2="23" y2="16"></line></svg>Multitool (Made by Eclipse)'; // set the text of the button
-    rpBtn.setAttribute("class", "btn flex gap-2 justify-center btn-neutral");
+    multiBtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" fill="none" stroke="#FFFFFF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><line x1="4" y1="21" x2="4" y2="14"></line><line x1="4" y1="10" x2="4" y2="3"></line><line x1="12" y1="21" x2="12" y2="12"></line><line x1="12" y1="8" x2="12" y2="3"></line><line x1="20" y1="21" x2="20" y2="16"></line><line x1="20" y1="12" x2="20" y2="3"></line><line x1="1" y1="14" x2="7" y2="14"></line><line x1="9" y1="8" x2="15" y2="8"></line><line x1="17" y1="16" x2="23" y2="16"></line></svg>Multitool (Made by Eclipse)'; // set the text of the button
+    multiBtn.setAttribute("class", "btn flex gap-2 justify-center btn-neutral");
     btns.push(multiBtn);
 
     btnArray.appendChild(jailbreakBtn);
